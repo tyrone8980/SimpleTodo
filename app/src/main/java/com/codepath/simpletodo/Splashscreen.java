6732989c783abe29934f99db3904c1f3e7645e7d
tyrone8980 <harmontyrone@gmail.com>
2018-10-30 08:12:26 -0400

    * Splash Screen

---
+++ app/src/main/java/com/codepath/simpletodo/Splashscreen.java
@@ -0,0 +1,25 @@
+package com.codepath.simpletodo;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.view.WindowManager;
+
+public class Splashscreen extends AppCompatActivity {
+    private static int SPLASH_TIME_OUT = 3000;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_splashscreen);
+        new Handler().postDelayed(new Runnable(){
+            @Override
+            public void run(){
+                Intent homeIntent = new Intent(Splashscreen.this, MainActivity.class);
+                startActivity(homeIntent);
+                finish();
+            }
+        },SPLASH_TIME_OUT);
+    }
+}
